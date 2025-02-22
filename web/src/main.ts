fa0ee8e9e5f06c17c2b7a0684ae82a46eae956d6
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2025-02-22 19:05:28 -0300

    feat: init web app

---
+++ web/src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+import router from '@/routes'
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+const app = createApp(App)
+
+registerPlugins(app)
+app.use(router)
+
+app.mount('#app')
